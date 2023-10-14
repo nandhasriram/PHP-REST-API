9336d375b27a5891430523009884c0e5a4c4886b
nandhasriram <113893222+nandhasriram@users.noreply.github.com>
2023-10-14 12:25:28 +0530

    Add files via upload

---
+++ employees/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+
+include('function.php');
+$requestMethod=$_SERVER["REQUEST_METHOD"];
+
+if($requestMethod=="DELETE"){
+
+        $deleteEmployee=deleteEmployee($_GET);
+        echo $deleteEmployee;
+    
+}
+else
+{
+    $data=[
+        'status'=>405,
+        'message'=>$requestMethod, 'Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
